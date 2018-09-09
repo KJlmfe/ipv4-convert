f43f6b9a93901b2e73f79cdcc8c3f033fa81b08b
KJlmfe <kjlmfe@gmail.com>
2018-09-09 15:23:46 +0800

    feat: implement convertIPv4StrToInt() without native api(split,trim):

--- index.js
+++ index.js
@@ -35,4 +35,59 @@ function convertIPv4StrToInt(ipv4Str) {
     return ipv4Int;
 }
 
+// Implement without native api(split, trim) and only iterate the string once.
+function convertIPv4StrToInt2(ipv4Str) {
+    if (typeof ipv4Str !== "string") {
+        throw TypeError("Illegal arguments, ipv4Str should be a string");
+    }
+
+    let ipv4Int = 0,
+        base = 1,
+        digit,
+        digitBase = 1,
+        dotsCnt = 0,
+        isSpaceAppearBeforeDigit = false,
+        char;
+
+    for (let i = ipv4Str.length - 1; i >= -1; i--) {
+        char = ipv4Str[i];
+
+        if (char === '.' || i === -1) { // dot or end
+            dotsCnt = i !== -1 ? dotsCnt + 1 : dotsCnt;
+            if (dotsCnt > 3 || (i === -1 && dotsCnt !== 3)) {
+                throw Error("Invalid IPv4 address, it should be formated in A.B.C.D");
+            }
+            if (digit === undefined) { // nothing between two dots, such as: 172.1..
+                throw Error("Invalid IPv4 address, each block (A.B.C.D) should be an integer");
+            }
+            if (digit >= 256) {
+                throw Error("Invalid IPv4 address, each block (A.B.C.D) should be less than 256");
+            }
+            ipv4Int += digit * base;
+            base *= 256;
+
+            digit = undefined;
+            digitBase = 1;
+            isSpaceAppearBeforeDigit = false;
+        } else if (/\d/.test(char)) { // digit
+            if (isSpaceAppearBeforeDigit) { // spaces between two digits, such as: 172.1.1 2
+                throw Error("Invalid IPv4 address, each block (A.B.C.D) should be an integer");
+            }
+
+            digit = parseInt(char) * digitBase + (digit === undefined ? 0 : digit);
+            digitBase *= 10;
+        } else if (char === ' ') { // space
+            if (digit !== undefined) {
+                isSpaceAppearBeforeDigit = true;
+            }
+
+            isSpaceAppear = true;
+        } else {
+            throw Error("Invalid IPv4 address, each block (A.B.C.D) should be an integer");
+        }
+    }
+
+    return ipv4Int;
+}
+
 module.exports = convertIPv4StrToInt;
